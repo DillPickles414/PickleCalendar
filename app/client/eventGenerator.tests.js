83cd75b22d9c8c4672a88bdabde7d674f700769a
acathers <acathers@hawaii.edu>
2020-03-13 00:35:30 -1000

    #21 added unit test for generateEvent

---
+++ app/client/eventGenerator.tests.js
@@ -0,0 +1,32 @@
+import chai from 'chai';
+import { formatDate, generateEvent } from './eventGenerator';
+
+const testData = {
+    eventName: 'testName',
+    dateStart: () => new Date(),
+    dateEnd: 'SET ME',
+    frequency: 'DAILY',
+    description: 'testDescription',
+    summary: 'testSummary',
+};
+
+
+describe('eventGenerator', function () {
+    it('output', function () {
+
+        const event = testData;
+        event.dateStart = new Date(event.dateStart());
+        event.dateEnd = event.dateStart;
+        event.dateEnd.setHours(event.dateStart.getHours() + 1);
+
+        const expectedString = `${'BEGIN:VCALENDAR\n' +
+        'BEGIN:VEVENT\n' +
+        'DESCRIPTION:'}${event.description}
+DTSTART:${formatDate(event.dateStart)}
+DTEND:${formatDate(event.dateEnd)}
+RRULE:FREQ=${event.frequency}
+SUMMARY;LANGUAGE=en-us:${event.summary}\nEND:VEVENT
+END:VCALENDAR`;
+        chai.assert.equal(generateEvent(event), expectedString);
+    });
+});
